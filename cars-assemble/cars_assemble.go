a6ca17490ef9ea75f76b053c0879e759e94f4063
Daniel Ruiz Camacho <daniel.ruizcamacho@gmail.com>
2022-04-10 11:25:01 +0200

    Updating cars assemble exercise

--- cars-assemble/cars_assemble.go
+++ cars-assemble/cars_assemble.go
@@ -1,43 +1,23 @@
 package cars
 
-// SuccessRate is used to calculate the ratio of an item being created without
-// error for a given speed
-func SuccessRate(speed int) float64 {
-    if speed == 0 {
-        return 0
-    }
-
-    if speed > 0 && speed <= 4 {
-        return 1.0
-    }
-
-    if speed > 4 && speed <= 8 {
-        return 0.9
-    }
-
-    return 0.77
+// CalculateWorkingCarsPerHour calculates how many working cars are
+// produced by the assembly line every hour
+func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float64 {
+	noOfCarProduced := float64((successRate / 100) * float64(productionRate))
+	return noOfCarProduced
 }
 
-// CalculateProductionRatePerHour for the assembly line, taking into account
-// its success rate
-func CalculateProductionRatePerHour(speed int) float64 {
-	return float64(221 * speed) * SuccessRate(speed)
-}
-
-// CalculateProductionRatePerMinute describes how many working items are
+// CalculateWorkingCarsPerMinute calculates how many working cars are
 // produced by the assembly line every minute
-func CalculateProductionRatePerMinute(speed int) int {
-    return int(CalculateProductionRatePerHour(speed) / 60)
+func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
+	carsProducedPerMin := int(((successRate / 100) * float64(productionRate)) / 60)
+	return carsProducedPerMin
 }
 
-// CalculateLimitedProductionRatePerHour describes how many working items are
-// produced per hour with an upper limit on how many can be produced per hour
-func CalculateLimitedProductionRatePerHour(speed int, limit float64) float64 {
-	calculatedProduction := CalculateProductionRatePerHour(speed)
-
-    if calculatedProduction > limit {
-        return limit
-    }
-
-    return calculatedProduction
+// CalculateCost works out the cost of producing the given number of cars
+func CalculateCost(carsCount int) uint {
+	individual := carsCount % 10
+	group := int(carsCount / 10)
+	cost := uint((group * 95000) + (individual * 10000))
+	return cost
 }
