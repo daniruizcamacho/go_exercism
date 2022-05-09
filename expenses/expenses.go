65554bea3790abe0da111ffaf87e47d32694eb4f
Daniel Ruiz Camacho <daniel.ruizcamacho@gmail.com>
2022-05-09 23:25:02 +0200

    Expenses exercise first class functions

---
+++ expenses/expenses.go
@@ -0,0 +1,82 @@
+package expenses
+
+import "errors"
+
+// Record represents an expense record.
+type Record struct {
+	Day      int
+	Amount   float64
+	Category string
+}
+
+// DaysPeriod represents a period of days for expenses.
+type DaysPeriod struct {
+	From int
+	To   int
+}
+
+// Filter returns the records for which the predicate function returns true.
+func Filter(in []Record, predicate func(Record) bool) []Record {
+	var filteredRecords []Record
+	for _, record := range in {
+		if predicate(record) {
+			filteredRecords = append(filteredRecords, record)
+		}
+	}
+
+	return filteredRecords
+}
+
+// ByDaysPeriod returns predicate function that returns true when
+// the day of the record is inside the period of day and false otherwise
+func ByDaysPeriod(p DaysPeriod) func(Record) bool {
+	return func(r Record) bool {
+		return r.Day >= p.From && r.Day <= p.To
+	}
+}
+
+// ByCategory returns predicate function that returns true when
+// the category of the record is the same as the provided category
+// and false otherwise
+func ByCategory(c string) func(Record) bool {
+	return func(r Record) bool {
+		return r.Category == c
+	}
+}
+
+// TotalByPeriod returns total amount of expenses for records
+// inside the period p
+func TotalByPeriod(in []Record, p DaysPeriod) float64 {
+	filteredRecords := Filter(in, ByDaysPeriod(p))
+	totalByPeriod := 0.0
+	for _, filteredRecord := range filteredRecords {
+		totalByPeriod += filteredRecord.Amount
+	}
+
+	return totalByPeriod
+}
+
+// CategoryExpenses returns total amount of expenses for records
+// in category c that are also inside the period p.
+// An error must be returned only if there are no records in the list that belong
+// to the given category, regardless of period of time.
+func CategoryExpenses(in []Record, p DaysPeriod, c string) (float64, error) {
+	filteredRecordsByDay := Filter(in, ByDaysPeriod(p))
+
+	if len(filteredRecordsByDay) == 0 {
+		return 0.0, nil
+	}
+
+	filteredRecordsByCategoryAndDay := Filter(filteredRecordsByDay, ByCategory(c))
+
+	if len(filteredRecordsByCategoryAndDay) == 0 {
+		return 0.0, errors.New("No records in list")
+	}
+
+	totalByCategoryAndDay := 0.0
+	for _, filteredRecord := range filteredRecordsByCategoryAndDay {
+		totalByCategoryAndDay += filteredRecord.Amount
+	}
+
+	return totalByCategoryAndDay, nil
+}
